cde715d84ec798cd7e3b067fd947169d9aa2c09f
dasistperfekto@gmail.com <dasistperfekto@gmail.com>
2021-03-28 14:55:05 +0300

    наследование

---
+++ nasledovanie/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace nasledovanie
+{
+    class Program
+    {
+        static void PrinFull(Person p)
+        {
+            Console.WriteLine(p.FirstName + " " + p.LastName);
+        }
+        static void Main(string[] args)
+        {
+            Person person = new Person("влад", "булахов");
+            person.PrintName();
+            Student student = new Student("рома", "бочкарев", 20);
+            student.PrintName();
+            student.Lern();
+            PrinFull(student);
+
+        }
+    }
+    class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Person(string name, string lest)
+        {
+            FirstName = name;
+            LastName = lest;
+        }
+        public void PrintName()
+        {
+            Console.WriteLine("меня зовут " + FirstName);
+        }
+    }
+    class Student : Person
+    {
+        public int Age { get; set; }
+        public Student(string name, string lest, int age) : base(name, lest)
+        {
+            Age = age;
+        }
+
+        public void Lern()
+        {
+            Console.WriteLine("я учусь");
+        }
+
+    }
+
+
+}
