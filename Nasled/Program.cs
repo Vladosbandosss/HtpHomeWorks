563f792aba143ff3ac77069fdebe1cf4978d6045
dasistperfekto@gmail.com <dasistperfekto@gmail.com>
2021-03-28 14:47:07 +0300

    наслед

--- Nasled/Program.cs
+++ Nasled/Program.cs
@@ -2,11 +2,52 @@
 
 namespace Nasled
 {
+    
     class Program
     {
+        static void PrinFull(Person p)
+        {
+            Console.WriteLine(p.FirstName + " " +  p.LastName);
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("nasled");
+            Person person = new Person("влад", "булахов");
+            person.PrintName();
+            Student student = new Student("рома", "бочкарев", 20);
+            student.PrintName();
+            student.Lern();
+            PrinFull(student);
+            
         }
     }
+    class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Person(string name,string lest)
+        {
+            FirstName = name;
+            LastName = lest;
+        }
+        public void PrintName()
+        {
+            Console.WriteLine("меня зовут " + FirstName);
+        }
+    }
+    class Student : Person
+    {
+        public int Age { get; set; }
+        public Student(string name, string lest,int age) : base(name, lest)
+        {
+            Age = age;
+        }
+
+        public void Lern()
+        {
+            Console.WriteLine("я учусь");
+        }
+
+    }
+  
+    
 }
