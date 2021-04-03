43ba6e2c90dacc548793ae2ccb0b4a2f83e49af8
dasistperfekto@gmail.com <dasistperfekto@gmail.com>
2021-04-03 20:08:42 +0300

    тсключения

---
+++ iskluchenia/MyOwnException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace iskluchenia
+{
+    class MyOwnException : Exception
+    {
+        public MyOwnException():base("это мое исключение")
+        {
+            
+        }
+        public MyOwnException(string mes):base(mes)
+        {
+            
+        }
+
+    }
+}
---
+++ iskluchenia/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace iskluchenia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+               
+                throw new MyOwnException();
+            }
+            catch(MyOwnException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch(DivideByZeroException ex)
+            {
+                Console.WriteLine("деление на 0");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Какое то исключение");
+            }
+            finally
+            {
+                Console.WriteLine("Работа завершена");
+            }
+        }
+    }
+}
