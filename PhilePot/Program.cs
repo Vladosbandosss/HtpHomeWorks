fe452da3ca223ca18db9d3c718b316cc280612b4
dasistperfekto@gmail.com <dasistperfekto@gmail.com>
2021-04-10 21:26:37 +0300

    файлы

---
+++ PhilePot/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace PhilePot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var sw=new StreamWriter("test.txt",false))//true будем пост добавлять езе кодировку можно добавить
+            {
+                sw.Write("Hello");
+                sw.WriteLine();
+                sw.Write("i am tom");
+            }
+            using(var sr=new StreamReader("test.txt"))//все считали
+            {
+                var text = sr.ReadToEnd();
+                Console.WriteLine(text);
+            }
+
+            using (var sr = new StreamReader("test.txt"))//счит по строкам
+            {
+                while (!sr.EndOfStream)
+                {
+                    Console.WriteLine(sr.ReadLine()+ " конец строки");
+                }
+               
+            }
+
+        }
+    }
+}
